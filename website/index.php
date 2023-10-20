f36535275f8640193f1ba91179455f38096a7237
GHKAK <87258919+GHKAK@users.noreply.github.com>
2023-10-20 03:35:50 +0300

    main layout

---
+++ website/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галактический вестник</title>
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <a href="#" class="brand">
+                <img class="brand-logo" src="./assets/images/logo.svg" alt="">
+                <h1 class="brand-sitename">Галактический вестник</h1>
+            </a>
+        </div>
+    </header>
+
+    <section class="banner">
+        <div class="container">
+            <div class="banner-article">
+                <h2 class="article-title">Возвращение этнографа</h2>
+                <p class="article-description">Сегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="news">
+        <div class="container">
+
+            <h2 class="news-title">Новости</h2>
+            <ul class="news-list">
+                <li class="news-card">
+                    <div class="card-date">11.06.2412</div>
+                    <div class="card-title">Возвращение этнографа</div>
+                    <p class="card-description">Сегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.
+                    </p>
+                    <a href="" class="card-button">
+                        <span class="button-name">Подробнее</span>
+                        <svg class="button-arrow" width="27" height="16" viewBox="0 0 27 16" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path id="Arrow"
+                                d="M1 7C0.447715 7 4.82823e-08 7.44772 0 8C-4.82823e-08 8.55228 0.447715 9 1 9L1 7ZM26.707 8.70711C27.0975 8.31658 27.0975 7.68342 26.707 7.2929L20.343 0.928934C19.9525 0.538409 19.3193 0.538409 18.9288 0.928934C18.5383 1.31946 18.5383 1.95262 18.9288 2.34315L24.5857 8L18.9288 13.6569C18.5383 14.0474 18.5383 14.6805 18.9288 15.0711C19.3193 15.4616 19.9525 15.4616 20.343 15.0711L26.707 8.70711ZM1 9L25.9999 9L25.9999 7L1 7L1 9Z"
+                                fill="#841844" />
+                        </svg>
+                    </a>
+                </li>
+                <li class="news-card">
+                    <div class="card-date">11.06.2412</div>
+                    <div class="card-title">Несчастный случай с известной светской дивой</div>
+                    <p class="card-description">
+                        Сегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.
+                    </p>
+                    <a href="" class="card-button">
+                        <span class="button-name">Подробнее</span>
+                        <img class="button-arrow" src="./assets/images/arrow.svg" alt="">
+                    </a>
+                </li>
+                <li class="news-card">
+                    <div class="card-date">11.06.2412</div>
+                    <div class="card-title">Папа Римский совершил визит в систему Альфы Центавра</div>
+                    <p class="card-description">
+                        Папа Римский Жан-Клод XIV посетил с апостольским визитом систему Альфы Центавра. Он принял
+                        участие в ряде благотворительных мероприятий и совершил богослужение в соборе Ван-Дамм-де-Ури.
+                    </p>
+                    <a href="" class="card-button">
+                        <span class="button-name">Подробнее</span>
+                        <img class="button-arrow" src="./assets/images/arrow.svg" alt="">
+                    </a>
+                </li>
+                <li class="news-card">
+                    <div class="card-date">11.06.2412</div>
+                    <div class="card-title">На чемпионате по пустотной гребле победила команда с Фобоса</div>
+                    <p class="card-description">
+                        В поясе астероидов на проходившем в последние выходные этапе системного чемпионата по пустотной
+                        гребле команда с Фобоса одержала убедительную победу.
+                    </p>
+                    <a href="" class="card-button">
+                        <span class="button-name">Подробнее</span>
+                        <img class="button-arrow" src="./assets/images/arrow.svg" alt="">
+                    </a>
+                </li>
+
+            </ul>
+            <ul class="pagination">
+                <li class="pagination-page active">1</li>
+                <li class="pagination-page">2</li>
+                <li class="pagination-page">3</li>
+                <li class="pagination-next">
+                    <!-- <img class="next-arrow" src="./assets/images/arrow-next.svg" alt=""> -->
+                    <svg class="next-arrow" width="24" height="22" viewBox="0 0 24 22" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <g id="Frame 315">
+                            <path id="Arrow 1"
+                                d="M3 10C2.44772 10 2 10.4477 2 11C2 11.5523 2.44772 12 3 12L3 10ZM18.466 11.7071C18.8565 11.3166 18.8565 10.6834 18.466 10.2929L12.102 3.92893C11.7115 3.53841 11.0783 3.53841 10.6878 3.92893C10.2973 4.31946 10.2973 4.95262 10.6878 5.34315L16.3447 11L10.6878 16.6569C10.2973 17.0474 10.2973 17.6805 10.6878 18.0711C11.0783 18.4616 11.7115 18.4616 12.102 18.0711L18.466 11.7071ZM3 12L17.7589 12L17.7589 10L3 10L3 12Z"
+                                fill="#841844" />
+                        </g>
+                    </svg>
+
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="container">
+
+            <div class="footer-line"></div>
+            <p class="copyright">© 2023 — 2412 «Галактический вестник»</p>
+        </div>
+    </footer>
+</body>
+
+</html>
